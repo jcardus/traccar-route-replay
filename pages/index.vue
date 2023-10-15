e15e75b98851eae9d87aaf19b988fab499701ef6
jcardus <asklocation.net@gmail.com>
2023-10-15 01:06:48 +0100

    start speed control add 3d model

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div style="height: 100%; width: 100%">
     <loading :active="loading" />
     <div id="map" style="height: 100%; width: 100%" />
-    <div id="slider" ref="slider" class="maplibregl-ctrl maplibregl-ctrl-group mapboxgl-ctrl-timeline">
+    <div ref="slider" class="maplibregl-ctrl maplibregl-ctrl-group mapboxgl-ctrl-timeline">
       <div class="mapboxgl-ctrl-timeline__control">
         <button
           class="mapboxgl-ctrl-timeline__toggler"
@@ -22,6 +22,16 @@
         {{ timestamps[i] && new Date(timestamps[i]).toLocaleString() }}
       </div>
     </div>
+    <div ref="speed" class="maplibregl-ctrl maplibregl-ctrl-group">
+      <div style="padding: 5px" class="maplibregl-ctrl-icon">
+        <input id="1x" type="radio" name="speed">
+        <label for="1x">1x</label>
+        <input id="2x" type="radio" name="speed">
+        <label for="2x">2x</label>
+        <input id="5x" type="radio" name="speed">
+        <label for="5x">5x</label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,6 +43,7 @@ import bbox from '@turf/bbox'
 import { lineString } from '@turf/helpers'
 import { MapboxOverlay } from '@deck.gl/mapbox'
 import Loading from 'vue-loading-overlay'
+import { ScenegraphLayer } from '@deck.gl/mesh-layers'
 import { closest } from '@/utils'
 import { fitBounds } from '@/utils/options'
 
@@ -48,13 +59,14 @@ const props = {
   getPath: d => d.path,
   getTimestamps: d => d.timestamps,
   opacity: 0.9,
-  widthMinPixels: 8,
+  widthMinPixels: 4,
   rounded: true,
   joinRounded: true,
   trailLength: 0,
   shadowEnabled: true,
   getColor: [253, 128, 93]
 }
+const MODEL_URL = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/google-3d/truck.gltf'; // eslint-disable-line
 
 export default {
   name: 'IndexPage',
@@ -63,11 +75,12 @@ export default {
     return {
       loading: false,
       playing: false,
-      i: 0
+      i: 0,
+      playSpeed: 100
     }
   },
   computed: {
-    ...mapGetters(['devices', 'path', 'timestamps']),
+    ...mapGetters(['devices', 'path', 'timestamps', 'route']),
     currentTime: {
       get () { return this.timestamps[this.i] },
       set (time) { this.i = closest(this.timestamps, time) }
@@ -76,7 +89,21 @@ export default {
   watch: {
     i () {
       props.currentTime = this.timestamps[this.i]
-      overlay.setProps({ layers: [new TripsLayer(props)] })
+      overlay.setProps({
+        layers: [
+          new TripsLayer(props),
+          new ScenegraphLayer({
+            id: 'truck',
+            data: [{ point: this.path[this.i], heading: this.route[this.i].course }],
+            scenegraph: MODEL_URL,
+            sizeScale: 500,
+            getPosition: d => d.point,
+            getTranslation: [0, 0, 1],
+            getOrientation: d => [0, 180 - d.heading, 90],
+            _lighting: 'pbr'
+          })
+        ]
+      })
     },
     path () {
       this.loading = false
@@ -111,30 +138,33 @@ export default {
     map.on('load', () => this.$store.dispatch('getPath'))
     map.addControl(new maplibregl.NavigationControl())
     map.addControl({ onAdd: () => this.$refs.slider }, 'top-left')
+    map.addControl({ onAdd: () => this.$refs.speed }, 'top-left')
     map.addControl(overlay)
   },
   methods: {
     addLayers () {
-      if (!map.getSource('route')) {
-        map.addSource('route', {
-          type: 'geojson',
-          data: lineString(this.path)
-        })
-      } else {
-        map.getSource('route').setData()
-      }
-      if (!map.getLayer('route')) {
-        map.addLayer({
-          id: 'route',
-          type: 'line',
-          source: 'route'
-        })
+      if (this.path && this.path.length) {
+        if (!map.getSource('route')) {
+          map.addSource('route', {
+            type: 'geojson',
+            data: lineString(this.path)
+          })
+        } else {
+          map.getSource('route').setData()
+        }
+        if (!map.getLayer('route')) {
+          map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: 'route'
+          })
+        }
       }
     },
     play () {
       if (this.i + 1 < this.path.length && this.playing) {
         this.i++
-        window.requestAnimationFrame(this.play)
+        setTimeout(window.requestAnimationFrame, this.playSpeed, this.play)
       } else {
         this.playing = false
       }
