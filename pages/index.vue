d67836b9b3627ced8ce6504691a9a80f5cf94ef1
jcardus <asklocation.net@gmail.com>
2023-10-22 05:10:34 +0100

    fix static urls

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,17 @@
       <div class="mapboxgl-ctrl-timeline__control" style="flex-grow: 1">
         <button
           class="mapboxgl-ctrl-icon svg-button"
-          style="background-image: url('/backward.svg')"
+          style="background-image: url('backward.svg')"
           @click="(i - 1 >= 0) && i--"
         />
         <button
           class="mapboxgl-ctrl-icon svg-button"
-          :style="`background-image: url('/${playing ? 'pause' : 'play'}.svg')`"
+          :style="`background-image: url('${playing ? 'pause' : 'play'}.svg')`"
           @click="playing = !playing"
         />
         <button
           class="mapboxgl-ctrl-icon svg-button"
-          style="background-image: url('/forward.svg')"
+          style="background-image: url('forward.svg')"
           @click="(i + 1 < path.length) && i++"
         />
         <input
