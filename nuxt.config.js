b228a8c4c59c12f94170867ae110daa4c45aeda0
jcardus <asklocation.net@gmail.com>
2024-02-12 01:44:29 +0000

    1,2,5

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.BASE_URL || 'http://localhost:8082/api',
+    baseURL: process.env.BASE_URL || '/api',
     credentials: true
   },
 
